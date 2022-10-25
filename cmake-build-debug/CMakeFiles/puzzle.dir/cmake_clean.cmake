200159ba228359a73e40abcb9908c0a1851e08aa
andonealexandru <andonealexandru19@yahoo.com>
2022-10-25 09:33:57 +0300

    Added cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/puzzle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/puzzle.dir/main.cpp.obj"
+  "libpuzzle.dll.a"
+  "puzzle.exe"
+  "puzzle.exe.manifest"
+  "puzzle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/puzzle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
